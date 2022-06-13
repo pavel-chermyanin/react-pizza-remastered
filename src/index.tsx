82358472f6bf31fafc6ecac095d502df362ab971
Pavel <pavel_chermyanin@mail.ru>
2022-06-13 20:42:17 +0500

    changed browser-router on hash-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from './redux/store';
 import { Provider } from 'react-redux'
 
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(rootElem);
 
 root.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
 );
 
